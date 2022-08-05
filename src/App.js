320e4ad751bd70daa15d3abed0311ca273e6e865
Leobardo Verdugo <leobardo.21@outlook.es>
2022-08-05 03:49:36 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Main/Home";
+import { Detailed } from "./components/Main/Detailed";
+import { useState, useEffect } from "react";
+import { getAll } from "./utils.js";
 function App() {
+  const [data, setData] = useState();
+  const [dark, setDark] = useState(
+    localStorage.getItem("dark") === "true" ? true : false
+  );
+
+  // console.log(localStorage.getItem("dark"));
+  useEffect(() => {
+    const getData = async () => {
+      setData(await getAll());
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${dark ? "dark" : ""}`}>
+      <div
+        className={`flex flex-col min-h-screen h-full antialiased dark:bg-dark-fe`}
+      >
+        <Header dark={dark} setDark={setDark} />
+        <Routes>
+          <Route path="/" element={<Home data={data} />} />
+          <Route path="/country/:id" element={<Detailed data={data} />} />
+        </Routes>
+      </div>
     </div>
   );
 }
